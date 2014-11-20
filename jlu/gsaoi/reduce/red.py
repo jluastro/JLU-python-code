5840d9329bbf4c77e39b0f1dc60d1665b7f3281d
Max Service <mws83@case.edu>
2014-11-19 19:44:50 -1000

    Eliminated Changing the directory

--- jlu/gsaoi/reduce/red.py
+++ jlu/gsaoi/reduce/red.py
@@ -94,7 +94,7 @@ def red_dir(directory,clean_dir, sky_key='sky', flat_key='Domeflat', sci_keys= [
     
     for i in frame_list:
         #import pdb; pdb.set_trace()
-        print >> all_f, directory+i
+        print >> all_f, directory+i+'.fits'
         head = fits.getheader(directory+i+'.fits')
         if head['OBJECT'] == sky_key:
             print >> sky_f, i
