cead2d1ddea1b33539bd2df3e71a0b7fb7df0273
Max Service <mws83@case.edu>
2014-11-21 14:21:30 -1000

    Fixed bugs in structure making

--- jlu/gsaoi/reduce/struc.py
+++ jlu/gsaoi/reduce/struc.py
@@ -58,10 +58,9 @@ def mk_struc(frame_file=None, directory=None, ret=False, day_diff=14, sci_keys= 
     
     while np.any(obs_bool):
         print num_done
-        
+        obs_breaks.append(np.argmin(mjd[obs_bool])+num_done-1)
         obs_bool = obs_bool * (mjd[obs_breaks[-1]]+14 < mjd)
         print obs_bool
-        obs_breaks.append(np.argmin(mjd[obs_bool])+num_done-1)
         for i in range(len(mjd)):
             num_done += obs_bool[i]
         epoch_bool_ars.append((mjd[obs_breaks[-1]]+14 > mjd) * (mjd[obs_breaks[-1]] < mjd))
