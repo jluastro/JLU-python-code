0a5ff9cafb39da9975406312ce293edfe903b62f
Max Service <mws83@case.edu>
2014-12-05 20:27:07 -1000

    added in copying reduce image back to reduce dir

--- jlu/gsaoi/reduce/red.py
+++ jlu/gsaoi/reduce/red.py
@@ -170,7 +170,7 @@ def red_dir(directory,clean_dir, sky_key='sky', flat_key='Domeflat', sci_keys= [
             
             iraf.imcopy('rg'+i+'['+str(k+1)+'][inherit+]' , 'rg'+i.replace('.fits',str(k+1)+'.fits'))
             shutil.move('rg'+i.replace('.fits',str(k+1)+'.fits'), clean_dir+'rg'+i.replace('.fits','_'+str(k+1)+'.fits'))
-            shutilcopy('rg'+i, directory+'rg'+i)
+            shutil.copy('rg'+i, directory+'rg'+i)
         os.remove('rg'+i)
     
     
