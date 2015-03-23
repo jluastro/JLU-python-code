b0a59652c0d0a7cd5079847a8e0b4ae9fe30ae7b
Max Service <mws83@case.edu>
2015-03-23 10:40:48 -1000

    added wd2 directory

--- jlu/wd2/org_lists.py
+++ jlu/wd2/org_lists.py
@@ -33,13 +33,17 @@ def match_named(label, starl_name):
 
     f.write('%13s  %6.3f  %8.3f  %10.5f  %10.5f  ' %
             (starlist['col1'][ml[0]], starlist['col2'][ml[0]], starlist['col3'][ml[0]],starlist['col4'][ml[0]], starlist['col5'][ml[0]]))
+    f.write('\n')
 
     f.write('%13s  %6.3f  %8.3f  %10.5f  %10.5f  ' %
             (starlist['col1'][ml[1]], starlist['col2'][ml[1]], starlist['col3'][ml[1]],starlist['col4'][ml[1]], starlist['col5'][ml[1]]))
+    f.write('\n')
+    
     for i in range(len(nameL)):
         if not i in  ml:
             f.write('%13s  %6.3f  %8.3f  %10.5f  %10.5f  ' %
                     (starlist['col1'][i], starlist['col2'][i], starlist['col3'][i],starlist['col4'][i], starlist['col5'][i]))
+            f.write('\n')
 
     f.close()
 
