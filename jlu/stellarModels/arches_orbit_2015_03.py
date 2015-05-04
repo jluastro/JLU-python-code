50c45f91e93d2aa7989a545d86545e5360f5a1c1
Matt <mwhosek@gmail.com>
2015-05-04 11:44:39 -1000

    First commit

---
+++ jlu/stellarModels/arches_orbit_2015_03.py
@@ -0,0 +1,32 @@
+import galpy
+import numpy as np
+import pylab as py
+from astropy.table import Table, vstack, Column
+from astropy.io import ascii
+import pyfits
+import pdb
+import arches_completeness_2015_03
+import arches_reddening_2015_03
+from jlu.stellarModels import extinction
+
+
+def define_potential():
+    """
+    Define potential for CMZ based on Launhardt+02
+    """
+    pass
+
+
+    return
+
+
+
+def run_orbits():
+    """
+    Calculate orbits presented in Stolte+08 using galpy
+    """
+    pass
+
+
+
+    return
