2f71638077ea1e61d4408b579f533ecf2599825b
HaynesStephens <hfstephens13@gmail.com>
2017-02-13 11:44:38 -0800

    added mb980006 object to analysis.py

--- jlu/microlens/analysis.py
+++ jlu/microlens/analysis.py
@@ -1,5 +1,55 @@
 from nirc2.reduce import analysis
 
+class MB980006(analysis.Analysis):
+    def __init__(self, epoch, filt, rootDir='/u/jlu/data/microlens/', 
+                 epochDirSuffix=None, cleanList='c.lis', alignMagCut=' -m 20 '):
+        """
+        For MB980006 reduction:
+
+        epoch -- '11may' for example
+        filt -- 'kp', 'lp', or 'h'
+        """
+        # Setup some W51a specific parameters
+        self.mapFilter2Cal = {'kp': 1, 'h': 2, 'j': 3}
+
+        filt_field = 'MB980006_' + filt
+
+        # Initialize the Analysis object
+        analysis.Analysis.__init__(self, epoch, filt=filt_field,
+                                     rootDir=rootDir, 
+                                     epochDirSuffix=epochDirSuffix,
+                                     cleanList=cleanList)
+        
+        # Use the field to set the psf starlist
+        self.starlist = self.rootDir + 'source_list/mb980006_psf.list'
+
+        ##########
+        # Setup the appropriate calibration stuff.
+        ##########
+        # Use the default stars
+        self.calStars = None
+
+        # Choose the column based on the filter
+        self.calColumn = self.mapFilter2Cal[filt]
+
+        # Set the coo star
+        self.cooStar = 'MB980006'
+        self.calCooStar = self.cooStar
+
+        # Override some of the default parameters
+        self.calFlags = '-f 1 -R -s 1 '
+        self.calFile = self.rootDir + 'source_list/mb980006_photo.dat'
+        
+        self.labellist = self.rootDir + 'source_list/mb980006_label.dat'
+        self.orbitlist = None
+
+        # Fix align flags. Otherwise, align is using too many faint stars.
+        self.alignFlags = '-R 3 -v -p -a 2 ' + alignMagCut
+
+        self.plotPosMagCut = 17.0
+        
+        return
+
 class OB110061(analysis.Analysis):
     def __init__(self, epoch, filt, rootDir='/u/jlu/data/microlens/', 
                  epochDirSuffix=None, cleanList='c.lis'):
