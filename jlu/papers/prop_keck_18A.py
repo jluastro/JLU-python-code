7ce82d23e2babe2cea80b2b17a05411a18f42714
Jessica Lu <jlu.astro@yahoo.com>
2017-09-22 12:37:00 -0700

    Fatima adds plots for proposal.

--- jlu/papers/prop_keck_18A.py
+++ jlu/papers/prop_keck_18A.py
@@ -11,15 +11,15 @@ def plot_airmass_moon():
     ra = "18:00:00"
     dec = "-30:00:00"
     months = np.array([4, 5, 6, 7])
-    days = np.array([10, 10, 10, 30])
+    days = np.array([23, 10, 10, 31])
     #outdir = '/Users/jlu/doc/proposals/keck/uc/18A/'
     outdir = '/Users/fatima/Desktop/'
     
     # Keck 2
-    skycalc.plot_airmass(ra, dec, 2018, months, days, 'keck2', outfile=outdir + 'microlens_airmass_keck2.png', date_idx=0)
+    skycalc.plot_airmass(ra, dec, 2018, months, days, 'keck2', outfile=outdir + 'microlens_airmass_keck2.png', date_idx=-1)
     skycalc.plot_moon(ra, dec, 2018, months, outfile=outdir + 'microlens_moon.png')
 
     # Keck 1
-    skycalc.plot_airmass(ra, dec, 2018, months, days, 'keck1', outfile=outdir + 'microlens_airmass_keck1.png', date_idx=0)
+    skycalc.plot_airmass(ra, dec, 2018, months, days, 'keck1', outfile=outdir + 'microlens_airmass_keck1.png', date_idx=-1)
     
     return
