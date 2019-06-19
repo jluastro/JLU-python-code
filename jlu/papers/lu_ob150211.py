d8f8b6b5a2889672c6a114f9a570f2b15195dd02
Nijaid <22627046+Nijaid@users.noreply.github.com>
2019-06-19 12:43:10 -0700

    Split on sky figure into two

--- jlu/papers/lu_ob150211.py
+++ jlu/papers/lu_ob150211.py
@@ -46,7 +46,7 @@ def setup():
     return data, mymodel, t_mod, tast, tphot
 
 def get_cmap(t_mod):
-    # Prep the colorbar                                                                                
+    # Prep the colorbar
     cmap = plt.cm.viridis
     norm = plt.Normalize(vmin=t_mod.min(), vmax=t_mod.max())
     smap = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
@@ -56,8 +56,8 @@ def get_cmap(t_mod):
 
 def mainplot():
     data, mymodel, t_mod, tast, tphot = setup()
-    
-    # Get the image and stars 
+
+    # Get the image and stars
     img = fits.getdata(combo_dir + 'mag17jun05_ob150211_kp.fits')
     psf_file = '/g/lu/data/microlens/source_list/ob150211_psf.list'
     psf_tab = Table.read(psf_file, format='ascii', header_start=-1)
@@ -179,13 +179,13 @@ def mainplot():
     plt.savefig(paper_dir + 'ob150211_mainplot.pdf')
     plt.show()
 
-def plot_onSky():
+def plot_onSky(plot_residuals=True):
     '''
     Plots the on-sky motion (unlensed motion subtracted from the astrometry)
     of the centroid, with side plots of the residuals of the unlensed observations to the model.
     '''
-    data, mymodel, t_mod = setup()[:3]
-    
+    data, mymodel, t_mod, tast, _ = setup()
+
     # Model the unlensed motion
     dp_tmod_unlens = mymodel.get_astrometry(t_mod) - mymodel.get_astrometry_unlensed(t_mod)
     x_mod_no_pm = dp_tmod_unlens[:, 0]*-1
@@ -205,47 +205,46 @@ def plot_onSky():
 
     # PLOT
     cmap, norm, smap = get_cmap(t_mod)
-    plt.figure(figsize=(8,8))
+    plt.clf()
+    plt.figure(1)
 
-    # definitions for the axes
-    left, width = 0.13, 0.55
-    bottom, height = 0.11, 0.55
-    bottom_h = left_h = left + width + 0.04
-
-    axSky = plt.axes([left, bottom, width, height])
-    axdec = plt.axes([left, bottom_h - 0.02, width, 0.14])
-    axra = plt.axes([left_h, bottom, 0.14, height])
-
-    axSky.scatter(x_no_pm*1e3, y_no_pm*1e3, c=data['t_ast'],
+    plt.scatter(x_no_pm*1e3, y_no_pm*1e3, c=data['t_ast'],
                 cmap=cmap, norm=norm, s=10)
-    axSky.errorbar(x_no_pm*1e3, y_no_pm*1e3,
+    plt.errorbar(x_no_pm*1e3, y_no_pm*1e3,
                  xerr=data['xpos_err']*1e3, yerr=data['ypos_err']*1e3,
                  fmt='none', ecolor=smap.to_rgba(data['t_ast']))
-    axSky.scatter(x_mod_no_pm*1e3, y_mod_no_pm*1e3, c=t_mod, cmap=cmap, norm=norm, s=4)
-    axSky.invert_xaxis()
-    axSky.axis('equal')
-    axSky.set_xlabel(r'$\Delta \alpha^*$ (mas)')
-    axSky.set_ylabel(r'$\Delta \delta$ (mas)')
-
-    axra.scatter((x_mod_no_pm - x_mod_no_pm), t_mod, c=t_mod, cmap=cmap, norm=norm, s=2)
-    axra.errorbar((x_no_pm - x_no_pm_tdat)*1e3, data['t_ast'], xerr = data['xpos_err']*1e3,
-                 fmt='k.', ecolor=smap.to_rgba(data['t_ast']))
-    axra.set_ylabel('time (MJD)')
-    axra.yaxis.set_label_position('right')
-    axra.yaxis.tick_right()
-    # axra.yaxis.set_tick_params(rotation=-25)
-
-    axdec.scatter(t_mod, (y_mod_no_pm - y_mod_no_pm), c=t_mod, cmap=cmap, norm=norm, s=2)
-    axdec.errorbar(data['t_ast'], (y_no_pm - y_no_pm_tdat)*1e3, yerr = data['ypos_err']*1e3,
-                 fmt='k.', ecolor=smap.to_rgba(data['t_ast']))
-    axdec.set_xlabel('time (MJD)')
-    axdec.xaxis.set_label_position('top')
-    axdec.xaxis.tick_top()
-    # axdec.locator_params(axis='x', nbins=6)
-    # axdec.xaxis.set_tick_params(rotation=25)
+    plt.scatter(x_mod_no_pm*1e3, y_mod_no_pm*1e3, c=t_mod, cmap=cmap, norm=norm, s=4)
+    plt.gca().invert_xaxis()
+    plt.axis('equal')
+    plt.xlabel(r'$\Delta \alpha^*$ (mas)')
+    plt.ylabel(r'$\Delta \delta$ (mas)')
 
     plt.savefig(paper_dir + 'ob150029_onsky.pdf')
     plt.show()
+
+    if plot_residuals:
+        # Get indices to isolate astrometry time
+        residx = np.where((t_mod>=tast.min()) & (t_mod<=tast.max()))[0]
+
+        figres, (axra, axdec) = plt.subplots(2, 1, sharex=True, num=2)
+
+        axra.scatter(t_mod[residx], (x_mod_no_pm - x_mod_no_pm)[residx],
+                     c=t_mod[residx], cmap=cmap, norm=norm, s=2)
+        axra.errorbar(data['t_ast'], (x_no_pm - x_no_pm_tdat)*1e3, yerr = data['xpos_err']*1e3,
+                          fmt='k.', ecolor=smap.to_rgba(data['t_ast']), elinewidth=2)
+        axra.set_ylabel(r'residuals in $\alpha^*$ (mas)')
+
+        axdec.scatter(t_mod[residx], (y_mod_no_pm - y_mod_no_pm)[residx],
+                      c=t_mod[residx], cmap=cmap, norm=norm, s=2)
+        axdec.errorbar(data['t_ast'], (y_no_pm - y_no_pm_tdat)*1e3, yerr = data['ypos_err']*1e3,
+                           fmt='k.', ecolor=smap.to_rgba(data['t_ast']), elinewidth=2)
+        axdec.set_xlabel('time (MJD)')
+        axdec.set_ylabel(r'residuals in $\delta$ (mas)')
+
+        figres.subplots_adjust(hspace=0)
+
+        plt.savefig(paper_dir + 'ob150029_onsky_res.pdf')
+        plt.show()
 
 def plot_obs():
     '''
@@ -254,7 +253,7 @@ def plot_obs():
     Centers the astrometry at 17jun05.
     '''
     data, mymodel, t_mod, tast, tphot = setup()
-    
+
     # Set the (0,0) point at 17jun05
     xcenter = data['xpos'][7]
     ycenter = data['ypos'][7]
@@ -286,7 +285,7 @@ def plot_obs():
     ax[1, 0].invert_yaxis()
     ax[1, 0].set_xlabel('time (MJD)')
     ax[1, 0].set_ylabel('residuals')
-    
+
     ax[0, 1].scatter(tast, xpos, c=tast, cmap=cmap, norm=norm, s=2)
     ax[0, 1].errorbar(data['t_ast'], (data['xpos'] - xcenter)*-1e3, yerr=data['xpos_err']*1e3, fmt='k.')
     ax[0, 1].set_ylabel(r'$\Delta \alpha^*$ (mas)')
@@ -310,6 +309,6 @@ def plot_obs():
     ax[1, 2].set_ylabel('residuals')
 
     fig.subplots_adjust(left=0.08, hspace=0, wspace=0.35)
-    
+
     plt.savefig(paper_dir + 'ob150211_data.pdf')
     plt.show()
