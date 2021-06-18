c6b5e01b33efbc8fbeed02f0b284486e5f5e07c7
Casey Lam <casey_lam@berkeley.edu>
2021-06-17 17:41:41 -0700

    added function to go from yearfraction to YYYY-MM-DD

--- jlu/util/datetimeUtil.py
+++ jlu/util/datetimeUtil.py
@@ -1,5 +1,6 @@
 import datetime
 import time
+import numpy as np 
 
 def toYearFraction(date):
     """
@@ -18,3 +19,19 @@ def toYearFraction(date):
     fraction = yearElapsed/yearDuration
 
     return date.year + fraction
+
+def toYYYYMMDD(yearfraction):
+    if np.isnan(yearfraction):
+        return 'YYYY-MM-DD'
+    else:
+        year = int(yearfraction)
+        rem = yearfraction - year
+        
+        base = datetime.datetime(year, 1, 1)
+        result = base + datetime.timedelta(seconds=(base.replace(year=base.year + 1) - base).total_seconds() * rem)
+        yyyy = result.year
+        mm = result.month
+        dd = result.day
+        yyyymmdd = '{0:0>4d}-{1:0>2d}-{2:0>2d}'.format(yyyy, mm, dd)
+    
+        return yyyymmdd
